86849a198f6a9dd0c557213d78b37b08f19a6e83
Amir Mosquera <amamosni@gmail.com>
2024-01-19 09:33:34 -0500

    Solucion ejercicio 2 ciber

--- backup-utility.sh
+++ backup-utility.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Directorio de origen que deseas respaldar
-origen="/ruta/al/directorio/a/respaldar"
+origen="/home/kali/bash-course"
 
 # Directorio de destino donde se almacenarán las copias de seguridad
-destino="/ruta/al/directorio/de/respaldo"
+destino="/home/kali/Backup-Utility/backup"
 
 # Nombre del archivo de copia de seguridad con marca de tiempo
-archivo_destino="$destino/respaldo_$(date +\%Y\%m\%d_\%H\%M\%S).tar.gz"
+archivo_destino="$destino/backup_$(date +\%Y\%m\%d_\%H\%M\%S).tar.gz"
 
 # Comprimir y copiar los archivos de origen al directorio de destino
 tar -czvf "$archivo_destino" "$origen"
@@ -18,3 +18,9 @@ if [ $? -eq 0 ]; then
 else
   echo "Error al realizar la copia de seguridad."
 fi
+
+#Copiar el archivo al servidor de copias de seguridad en la ip asignada
+#Se monto una maquina virtual de ubuntu a la cual se le instalo y configuro
+#vsftpd
+curl -T "$archivo_destino" -u ubuntu:ubuntu ftp://192.168.31.34/
+
